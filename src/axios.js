b745e1ba3c78c96ec3fb10171aa49a1e18b86354
Shruti Semwal <semwalshruti450@gmail.com>
2021-07-17 15:29:52 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,8 +3,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://us-central1-clone-e5f5d.cloudfunctions.net/api'
-    //'http://localhost:5001/clone-e5f5d/us-central1/api'  //the API (cloud function) url
+    baseURL: 'Your_cloud_function_url'
+      //the API (cloud function) url
 });
 
 export default instance;
